4bac86854a32e99207c4659a9c4381118fee8696
Sergey Kuzhlev <57180259+sergey-kuzhlev@users.noreply.github.com>
2022-04-30 16:06:27 +0300

    fix mytest2

--- mytest2.py
+++ mytest2.py
@@ -1,4 +1,4 @@
-def odometr(oksana: int) -> int:
+def odometer(oksana: int) -> int:
     """
     the function of finding the way
     according to the array data
@@ -6,5 +6,5 @@ def odometr(oksana: int) -> int:
     way = oksana[0] * (oksana[1])
     for i in range(2, len(oksana)):
         if i % 2 == 0:
-            way += oksana[i] * (oksana[i+1] - oksana[i-1])
+            way += oksana[i] * (oksana[i + 1] - oksana[i - 1])
     return way
